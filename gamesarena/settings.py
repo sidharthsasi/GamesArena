ddc23ce83a8c102c73f7e5252cbe326e1fdf4868
Sidharth Sasi <sidharthsasi04@gmail.com>
2022-07-12 11:21:12 +0530

    database chngd

--- gamesarena/settings.py
+++ gamesarena/settings.py
@@ -109,15 +109,27 @@ AUTH_USER_MODEL  = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "E-commercee",
-        "USER": "postgres",
-        "PASSWORD": "9889",
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "E-commercee",
+#         "USER": "postgres",
+#         "PASSWORD": "9889",
        
+#     }
+# }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
